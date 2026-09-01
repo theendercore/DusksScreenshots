b04dd521e777dfd29511daaede0bc6f94e01df29
TheEnderCore <theendercore@gmail.com>
2026-09-01 20:38:54 +0300

    deleted

--- src/client/java/com/theendercore/dusks/screenshots/mixin/client/ScreenshotMixin.java
+++ src/client/java/com/theendercore/dusks/screenshots/mixin/client/ScreenshotMixin.java
@@ -1,7 +1,6 @@
 package com.theendercore.dusks.screenshots.mixin.client;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
-import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.client.Screenshot;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -12,7 +11,7 @@ import java.io.File;
 public class ScreenshotMixin {
 
     @ModifyExpressionValue(method = "method_1664", at = @At(value = "INVOKE", target = "Ljava/io/File;getAbsolutePath()Ljava/lang/String;"))
-    private static String run2(String original, @Local(argsOnly = true) File file) {
+    private static String run2(String original, File file) {
         return file.getParent();
     }
 
